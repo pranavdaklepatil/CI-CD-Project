2359cd65d94c765f1a2efbd18842c41c81916be1
Pranav Dakle <pranvdakle15@gmail.com>
2026-03-26 10:29:08 +0530

    Created script fand  scannig k8s cluster

---
+++ Infrastrecture/scan_cluster.sh
@@ -0,0 +1,5 @@
+#Run it on the master node to scan the cluster and get the nodes information
+wget https://github.com/Shopify/kubeaudit/releases/download/v0.22.1/kubeaudit_0.22.1_linux_amd64.tar.gz
+tar -xvzf kubeaudit_0.22.1_linux_amd64.tar.gz
+sudo mv kubeaudit /usr/local/bin/
+kubeaudit all
