8cfe64ea50a5a59bb8e5dd0d99fedf760eca9ad1
Pranav Dakle <pranvdakle15@gmail.com>
2026-03-27 09:48:42 +0530

    updated typo

--- Infrastrecture/k8s_installation.sh
+++ Infrastrecture/k8s_installation.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt install docker.io -y
 sudo chmod 666 /var/run/docker.sock
 
-sudo apt-get inatsll -y apt-transport-https ca-certificates curl gpg
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 sudo mkdir -p -m 755 /etc/apt/keyrings
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
