fb9502d76400b856e3bcb3120992b511a5c3622a
Pranav Dakle <pranvdakle15@gmail.com>
2026-03-29 12:11:45 +0530

    added trivy to jenkins server

--- Infrastrecture/jenkins_setup.sh
+++ Infrastrecture/jenkins_setup.sh
@@ -75,9 +75,22 @@ sudo systemctl start docker
 sudo systemctl status docker --no-pager
 
 
+set -e
+sudo apt-get update
+sudo apt-get install -y wget apt-transport-https gnupg lsb-release
+
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/trivy.list
+
+sudo apt-get update
+sudo apt-get install -y trivy
+
+
+
 echo ""
 echo "✅ =============================================="
-echo "   Jenkins + Docker setup complete!"
+echo "   Jenkins + Docker + Trivy setup complete!"
 echo "================================================"
 echo ""
 echo "🌐 Access Jenkins at: http://$(curl -s ifconfig.me):8080"
